c9586c1c63eb49d2256f0b08263572d445266ddb
Shubhranshu Shekhar Shee <shubhranshushee569@gmail.com>
2025-10-23 20:09:46 +0530

    fix:debugged accountdao

--- src/main/java/org/groupprojects/securevault/dao/AccountDao.java
+++ src/main/java/org/groupprojects/securevault/dao/AccountDao.java
@@ -2,8 +2,6 @@ package org.groupprojects.securevault.dao;
 
 import org.groupprojects.securevault.model.Account;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class AccountDao {
 
@@ -13,147 +11,136 @@ public class AccountDao {
                 "jdbc:mysql://localhost:3306/smartbank_db", "root", "password");
     }
 
-    public boolean createAccount(Account account) {
-        String sql = "INSERT INTO personal_account (user_id, name, balance) VALUES (?, ?, ?)";
-        try (Connection con = getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
+    public Account getAccountByAccountNo(int accountNo) throws SQLException {
+        Account account = null;
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
 
+        try {
+            con = getConnection();
+            ps = con.prepareStatement("SELECT * FROM personal_account WHERE account_no = ?");
+            ps.setInt(1, accountNo);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                account = new Account();
+                account.setAccountNo(rs.getInt("account_no"));
+                account.setUserId(rs.getInt("user_id"));
+                account.setName(rs.getString("name"));
+                account.setBalance(rs.getDouble("balance"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) rs.close();
+            if (ps != null) ps.close();
+            if (con != null) con.close();
+        }
+
+        return account;
+    }
+
+    public Account getAccountByUserId(int userId) throws SQLException {
+        Account account = null;
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            ps = con.prepareStatement("SELECT * FROM personal_account WHERE user_id = ?");
+            ps.setInt(1, userId);
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                account = new Account();
+                account.setAccountNo(rs.getInt("account_no"));
+                account.setUserId(rs.getInt("user_id"));
+                account.setName(rs.getString("name"));
+                account.setBalance(rs.getDouble("balance"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) rs.close();
+            if (ps != null) ps.close();
+            if (con != null) con.close();
+        }
+
+        return account;
+    }
+
+    public boolean createAccount(Account account) throws SQLException {
+        boolean flag = false;
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = getConnection();
+            con.setAutoCommit(false);
+            ps = con.prepareStatement("INSERT INTO personal_account (user_id, name, balance) VALUES (?, ?, ?)");
             ps.setInt(1, account.getUserId());
             ps.setString(2, account.getName());
             ps.setDouble(3, account.getBalance());
 
-            return ps.executeUpdate() > 0;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public List<Account> getAccountsByUser(int userId) {
-        List<Account> accounts = new ArrayList<>();
-        String sql = "SELECT * FROM personal_account WHERE user_id=?";
-        try (Connection con = getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setInt(1, userId);
-
-            try (ResultSet rs = ps.executeQuery()) {
-                while (rs.next()) {
-                    Account account = mapResultSetToAccount(rs);
-                    accounts.add(account);
-                }
+            int i = ps.executeUpdate();
+            if (i > 0) {
+                flag = true;
             }
 
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        return accounts;
-    }
-
-    public Account getAccountByUserId(int userId) {
-        Account account = null;
-        String sql = "SELECT * FROM personal_account WHERE user_id = ?";
-        try (Connection con = getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setInt(1, userId);
-
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    account = mapResultSetToAccount(rs);
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return account;
-    }
-
-    public Account getAccountByAccountNo(int accountNo) {
-        Account account = null;
-        String sql = "SELECT * FROM personal_account WHERE account_no = ?";
-        try (Connection con = getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-
-            ps.setInt(1, accountNo);
-
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    account = mapResultSetToAccount(rs);
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return account;
-    }
-
-    public boolean depositMoney(int accountNo, double amount, String paymentMethod) {
-        Connection con = null;
-        try {
-            con = getConnection();
-            con.setAutoCommit(false);
-
-            Account account = getAccountByAccountNo(accountNo);
-            if (account == null) {
-                con.rollback();
-                return false;
-            }
-
-            String updateSql = "UPDATE personal_account SET balance = balance + ? WHERE account_no = ?";
-            try (PreparedStatement ps1 = con.prepareStatement(updateSql)) {
-                ps1.setDouble(1, amount);
-                ps1.setInt(2, accountNo);
-                ps1.executeUpdate();
-            }
-
-            String transactionSql = "INSERT INTO transactions (from_account, to_account, amount, transaction_date, transaction_type, payment_method, description) VALUES (?, ?, ?, NOW(), 'DEPOSIT', ?, ?)";
-            try (PreparedStatement ps2 = con.prepareStatement(transactionSql)) {
-                ps2.setInt(1, accountNo);
-                ps2.setInt(2, accountNo);
-                ps2.setDouble(3, amount);
-                ps2.setString(4, paymentMethod);
-                ps2.setString(5, "Money deposited via " + paymentMethod);
-                ps2.executeUpdate();
-            }
-
-            con.commit();
-            return true;
-
-        } catch (Exception e) {
-            try {
-                if (con != null) con.rollback();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-            e.printStackTrace();
-            return false;
         } finally {
             try {
-                if (con != null) {
-                    con.setAutoCommit(true);
-                    con.close();
+                if (flag == true) {
+                    con.commit();
+                } else {
+                    con.rollback();
                 }
-            } catch (SQLException e) {
+                if (ps != null) ps.close();
+                if (con != null) con.close();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+        return flag;
     }
 
-    public boolean depositMoney(int accountNo, double amount) {
-        return depositMoney(accountNo, amount, "ONLINE");
-    }
+    public boolean depositMoney(int accountNo, double amount) throws SQLException {
+        boolean flag = false;
+        Connection con = null;
+        PreparedStatement ps = null;
 
-    // Helper method to map ResultSet to Account object
-    private Account mapResultSetToAccount(ResultSet rs) throws SQLException {
-        Account account = new Account();
-        account.setAccountNo(rs.getInt("account_no"));
-        account.setUserId(rs.getInt("user_id"));
-        account.setName(rs.getString("name"));
-        account.setBalance(rs.getDouble("balance"));
-        return account;
+        try {
+            con = getConnection();
+            con.setAutoCommit(false);
+            ps = con.prepareStatement("UPDATE personal_account SET balance = balance + ? WHERE account_no = ?");
+            ps.setDouble(1, amount);
+            ps.setInt(2, accountNo);
+
+            int i = ps.executeUpdate();
+            if (i > 0) {
+                flag = true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (flag == true) {
+                    con.commit();
+                } else {
+                    con.rollback();
+                }
+                if (ps != null) ps.close();
+                if (con != null) con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return flag;
     }
 }
